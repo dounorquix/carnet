a8f17850f1f9731c23135d2a33f939d8f031ffe2
dounorquix <dounorquixsilva@gmail.com>
2023-06-13 16:08:40 -0400

    cambios con carnet

--- application/views/carnets.php
+++ application/views/carnets.php
@@ -5,10 +5,10 @@
 
 	  <div class="qr"> <img src="<?=base_url('public/img/qrcode/24433002.png')?>"></div>
 
-	  <div class="datos small"><p><?php print $tra->pri_nom.' '.$tra->pri_ape?></p><p name="ced" id="cedu"><?php print $tra->ced_tra;?></p></div>
+	  <div class="datos small"><p><?php print $tra->pri_nom.' '.$tra->pri_ape?></p><p name="ced" id="cedu"><?php print "V-".$tra->ced_tra;?></p></div>
 	  <div class="foto"><img style="border-radius: 100%; height: 120px; width: 120px;" src="<?=base_url('public/img/img.jpeg')?>"></div>
 	  <div class="cargo"><p><?php print $tra->nom_car;?></p></div>
-	  <div class="depe small"><p><?php print $tra->nom_dep;?></p></div>
+	  <div class="depe"><p><?php print $tra->nom_dep;?></p></div>
 	 
 	</div>
 </div>
